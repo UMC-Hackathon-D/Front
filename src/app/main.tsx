e0135c960a90ab41674460fe4b734415c0b5c590
BitedRadish <a980831@naver.com>
2024-12-21 23:23:23 +0900

    feat : RecoilRoot 추가

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,11 +4,14 @@ import { ThemeProvider } from "styled-components";
 import theme from "@app/styles/theme";
 import GlobalStyles from "@app/styles/globalStyles";
 import GlobalFonts from "@app/styles/globalFonts";
+import { RecoilRoot } from "recoil";
 
 createRoot(document.getElementById("root")!).render(
-    <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <GlobalFonts />
-        <Routing />
-    </ThemeProvider>
+    <RecoilRoot>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <GlobalFonts />
+            <Routing />
+        </ThemeProvider>
+    </RecoilRoot>
 );
